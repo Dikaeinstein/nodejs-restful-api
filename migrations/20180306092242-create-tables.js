5e3fa9d04f7b6dba7f504f4bb87569d040eca0d9
Dikaeinstein <Dikaeinstein@users.noreply.github.com>
2018-03-08 18:28:11 +0100

    fixed migration for create-tables

--- migrations/20180306092242-create-tables.js
+++ migrations/20180306092242-create-tables.js
@@ -12,69 +12,71 @@ exports.setup = (options, seedLink) => {
   seed = seedLink;
 };
 
-const createTodoItems = (database) => {
-  return database.createTable('todo_items', {
-    id: {
-      type: 'int',
-      notNull: true,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    content: { type: 'string', length: 255 },
-    complete: 'boolean',
-    todo_id: {
-      type: 'int',
-      foreignKey: {
-        name: 'todo_item_todo_id_fk',
-        table: 'todos',
-        rules: {
-          onUpdate: 'RESTRICT',
-          onDelete: 'CASCADE',
-        },
-        mapping: 'id',
-      },
-    },
-    createdAt: {
-      type: 'timestamp',
-      notNull: true,
-      defaultValue: (new Date()).toISOString(),
-    },
-    updatedAt: 'timestamp',
-  });
-};
-
-const createTodo = (database) => {
-  return database.createTable('todos', {
-    id: {
-      type: 'int',
-      primaryKey: true,
-      autoIncrement: true,
-      notNull: true,
-    },
-    title: { type: 'string', notNull: true, length: 255 },
-    user_id: {
-      type: 'int',
-      foreignKey: {
-        name: 'todo_user_id_fk',
-        table: 'users',
-        rules: {
-          onUpdate: 'RESTRICT',
-          onDelete: 'CASCADE',
-        },
-        mapping: 'id',
-      },
-    },
-    createdAt: {
-      type: 'timestamp',
-      notNull: true,
-      defaultValue: (new Date()).toISOString(),
-    },
-    updatedAt: 'timestamp',
-  });
-};
 
 exports.up = (db) => {
-  return createTodo(db)
+  const createTodoItems = (database) => {
+    return database.createTable('todo_items', {
+      id: {
+        type: 'int',
+        notNull: true,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      content: { type: 'string', length: 255 },
+      complete: 'boolean',
+      todo_id: {
+        type: 'int',
+        foreignKey: {
+          name: 'todo_item_todo_id_fk',
+          table: 'todos',
+          rules: {
+            onUpdate: 'RESTRICT',
+            onDelete: 'CASCADE',
+          },
+          mapping: 'id',
+        },
+      },
+      createdAt: {
+        type: 'timestamp',
+        notNull: true,
+        defaultValue: (new Date()).toISOString(),
+      },
+      updatedAt: 'timestamp',
+    });
+  };
+
+  const createTodos = (database) => {
+    return database.createTable('todos', {
+      id: {
+        type: 'int',
+        primaryKey: true,
+        autoIncrement: true,
+        notNull: true,
+      },
+      title: { type: 'string', notNull: true, length: 255 },
+      user_id: {
+        type: 'int',
+        foreignKey: {
+          name: 'todo_user_id_fk',
+          table: 'users',
+          rules: {
+            onUpdate: 'RESTRICT',
+            onDelete: 'CASCADE',
+          },
+          mapping: 'id',
+        },
+      },
+      createdAt: {
+        type: 'timestamp',
+        notNull: true,
+        defaultValue: (new Date()).toISOString(),
+      },
+      updatedAt: 'timestamp',
+    });
+  };
+
+
+  return createTodos(db)
     .then(result => createTodoItems(db))
     .catch(err => undefined);
 };
